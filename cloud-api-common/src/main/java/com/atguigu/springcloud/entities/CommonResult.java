9e5f9fe178f61b75bac95f5cc403543ccb5f8eb6
Administrator <zpf8836@163.com>
2021-03-07 21:02:17 +0800

    Add Comment

--- cloud-api-common/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-api-common/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -15,10 +15,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommonResult<T> {
 
+    //例如 404 NOT_FOUND
     private Integer code;
     private String message;
     private T data;
 
+    /**
+     * 只有编码和message的方法
+     */
     public CommonResult(Integer code, String message) {
         this(code, message, null);
     }
