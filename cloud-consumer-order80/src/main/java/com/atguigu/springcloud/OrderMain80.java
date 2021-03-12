c515289825f54b204fb641daeecddae834e8f114
Fred <zpf8836@163.com>
2021-03-12 22:49:10 +0800

    Add dependency
    Signed-off-by: Fred <zpf8836@163.com>

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -7,14 +7,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
- * @author zzyy
- * @date 2020/02/18 17:20
+ * @author zzyy 2020/02/18 17:20
  **/
 @SpringBootApplication
 @EnableEurekaClient
 @RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
     }
